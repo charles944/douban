e20ed4115b35229568597c5966477e401c7d88b3
charles <3390274844@qq.com>
2020-04-24 15:17:21 +0800

    third_modify

--- Auto/group.py
+++ Auto/group.py
@@ -1,3 +1,23 @@
 
-class group():
-    pass
+import requests
+from Auto.Settings import *
+from lxml import etree
+
+class Group(object):
+    def __init__(self,username,password,nickname):
+        self.username=username
+        self.password=password
+        self.nickname=nickname
+
+    def getGroups(username,nickname):
+        url=URL_HOST+nickname
+        resq=requests.get(url,headers=HEADERS)
+        HTML=etree.HTML(resq.text)
+        group_titles=HTML.xpath("")
+        group_urls=HTML.xpath("")
+        number_groups=HTML.xpath("")
+        return (number_groups,group_titles,group_urls)
+
+    def jiaruGroups(name_groups,url_groups,words,nickname):
+        for i in len(name_groups):
+            resq=requests.get
